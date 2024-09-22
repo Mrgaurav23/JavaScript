93765d27b96c94a7b3a049d714f6122ecfaa3748
Gaurav Sisodiya <121042555+Mrgaurav23@users.noreply.github.com>
2024-09-22 08:59:55 +0000

    coversionDT config

---
+++ 01_Basics/03_conversionDT.js
@@ -0,0 +1,62 @@
+/*
+part 1
+
+let score = 33;
+console.log(typeof (score));
+ // Number to String Conversion.
+let scorechange = String(score);
+console.log(typeof scorechange);
+
+let stringCheck = "ravindra";
+  //String to number Conversion.
+let convertNumber = Number(stringCheck);
+console.log(typeof convertNumber);
+
+*/
+
+/*
+part 2
+
+let score = "33abc"
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//Output: number
+//Output: NaN (Not A Number), 
+*/
+
+/*
+part3
+let score  = null;
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//output:number
+//output:0
+*/
+
+/*
+part4
+
+let score = true;
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//output:number
+//output:1
+*/
+
+/*
+part 5
+
+let isLoggedIn = 1;
+let booleanLoggedIN = Boolean(isLoggedIn);
+
+console.log(typeof booleanLoggedIN);
+console.log(booleanLoggedIN);
+// output: boolean
+// output: true
+*/
