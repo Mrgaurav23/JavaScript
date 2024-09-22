eb007409524a8f3440ff8b6dd7d01d29c21baa23
Gaurav Sisodiya <121042555+Mrgaurav23@users.noreply.github.com>
2024-09-22 07:39:40 +0000

    dtatype config

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,32 @@
+let name = "gaurav";
+let age = 19;
+let isLoggedIn = true;
+let email = null;
+let password;
+
+
+console.table([name,age,isLoggedIn,email,password])
+// Datatypes
+/*
+number => 2,
+bigInt => 356732,
+string => "",
+boolean => true/false,
+null => standAlone value means Empty,
+undefined => It is present in code but no value assigned to variable.
+symbol => unique
+*/
+
+//object
+
+console.log(typeof "gaurav") 
+// output: string
+
+console.log(typeof age)
+//output: number
+
+console.log(typeof null);
+//output: object
+
+console.log(typeof undefined);
+//output: undefined
