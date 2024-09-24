67ec4737fe769c9f8e6e3a5996b49c708a5f1ea4
Gaurav Sisodiya <121042555+Mrgaurav23@users.noreply.github.com>
2024-09-24 07:44:24 +0000

    object2 config

---
+++ 02_Basics/04_object2.js
@@ -0,0 +1,80 @@
+//Singleton.
+//make object with singleton
+const instaUser = new Object();
+console.log(instaUser);
+
+//Give information to object
+instaUser.id = "123abc";
+instaUser.name = "Ravindra";
+instaUser.age = "20";
+instaUser.isLoggedIn = false;
+console.log(instaUser);
+
+//Nested object 
+const anotherUser = {
+    email: "some@example.com",
+    fullName:{
+        userFullName:{
+            firstName:"Rohit",
+            lastName: "Sharma",
+        }
+    }
+}
+console.log(anotherUser.fullName.userFullName);
+
+//Merge two objects.
+const obj1 = {
+    1:"Ironman",
+    2:"SpiderMan"
+}
+const obj2 = {
+    3:"Hanuman",
+    4:"Shaktiman",
+}
+
+const obj3 = {
+    5:"SuperMan",
+    6:"Joker",
+}
+
+//This is not merge the object it gives object in objects.
+const obj4 = {
+    obj1,
+    obj2,
+}
+console.log(obj3);
+
+// Here we concat the object with assign.
+const obj5 = Object.assign({},obj1,obj2);
+console.log(obj4);
+
+// we also use spread operator just like array to cancat the object
+const obj6 = {...obj1,...obj2,...obj3};
+console.log(obj6);
+
+// object in arrar
+const user = [
+    {
+        id: 1,
+        email: "someexample@.com"
+    },
+    {
+        id: 2,
+        email: "someexample1@.com"
+    },
+    {
+        id: 3,
+        email: "someexample3@.com"
+    }
+]
+const user1Info = user[1].email;
+console.log(user1Info);
+
+//Give datatype array
+console.log(instaUser);
+console.log(Object.keys(instaUser));
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser));
+
+//key present or not in an object
+console.log(instaUser.hasOwnProperty('isLoggedIn'));
