9d650b5fdd1b6f82031a54951e2f56b61c70c193
Gaurav Sisodiya <121042555+Mrgaurav23@users.noreply.github.com>
2024-09-26 07:59:39 +0000

    scope config

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,53 @@
+//var is function scope.
+//let is blocked scope.
+
+let scope1 = 20;
+const scope2 = 30;
+var scope3 = 40;
+
+if(true){
+    let scope1 = 10;
+    const scope2 = 20;
+    // console.log("Inner :",scope1);
+    var scope3 = 30;
+}
+
+// console.log(scope1);
+// console.log(scope2);
+// console.log(scope3);
+
+function one(){
+    const userName = "Ravindra";
+
+    function two(){
+        const website = "Duo-Studio";
+        console.log(userName);
+    }
+    // console.log(website); give Error 
+
+    two();
+}
+// one();
+
+if(true){
+    const userName = "Ravindra";
+
+    if(userName === "Ravindra "){
+        let website = "Autono" 
+        console.log(userName + website);
+    }
+    // console.log(website); Give Error
+}
+
+// ++++++++++++++++++++++Interesting+++++++++++++++++++++//
+
+console.log(three(5));
+function three(num){
+    return num + 1;
+}
+
+const addTwo = function(num){
+    return num + 2;
+}
+console.log(addTwo(5));
+
